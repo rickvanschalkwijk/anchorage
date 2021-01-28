bca33eb2636beefc4b7f8fe81ccd7586938d37d8
Rick van Schalkwijk <rvanschalkwijk@mirabeau.nl>
2021-01-28 13:09:57 +0100

    first basic setup of the binder
    This class will bind literal, unary and binary expressions.

---
+++ ac/CodeAnalysis/Binding/Binder.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Anchorage.CodeAnalysis.Syntax;
+
+namespace Anchorage.CodeAnalysis.Binding
+{
+    internal enum BoundNodeKind
+    {
+        UnaryExpression,
+        LiteralExpression,
+        BinaryExpression
+    }
+
+    internal abstract class BoundNode
+    {
+        public abstract BoundNodeKind Kind { get; }
+    }
+
+    internal abstract class BoundExpression : BoundNode
+    {
+        public abstract Type Type { get; }
+    }
+
+    internal enum BoundUnaryOperatorKind
+    {
+        Identity,
+        Negation
+    }
+
+    internal sealed class BoundLiteralExpression : BoundExpression
+    {
+        public BoundLiteralExpression(object value)
+        {
+            Value = value;
+        }
+        public override BoundNodeKind Kind => BoundNodeKind.LiteralExpression;
+
+        public override Type Type => Value.GetType();
+
+        public object Value { get; }
+    }
+
+    internal sealed class BoundUnaryExpression : BoundExpression
+    {
+        public BoundUnaryExpression(BoundUnaryOperatorKind operatorKind, BoundExpression operand)
+        {
+            OperatorKind = operatorKind;
+            Operand = operand;
+        }
+
+        public override BoundNodeKind Kind => BoundNodeKind.UnaryExpression;
+
+        public override Type Type => Operand.Type;
+
+        public BoundUnaryOperatorKind OperatorKind { get; }
+
+        public BoundExpression Operand { get; }
+    }
+
+    internal sealed class BoundBinaryExpression : BoundExpression
+    {
+        public BoundBinaryExpression(BoundExpression left, BoundBinaryOperatorKind operatorKind, BoundExpression right)
+        {
+            Left = left;
+            OperatorKind = operatorKind;
+            Right = right;
+        }
+
+        public override BoundNodeKind Kind => BoundNodeKind.BinaryExpression;
+
+        public override Type Type => Left.Type;
+
+        public BoundExpression Left { get; }
+
+        public BoundBinaryOperatorKind OperatorKind { get; }
+
+        public BoundExpression Right { get; }
+    }
+
+    internal enum BoundBinaryOperatorKind
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    internal sealed class Binder
+    {
+        public BoundExpression BindExpression(ExpressionSyntax syntax)
+        {
+            return syntax.Kind switch
+            {
+                SyntaxKind.LiteralExpression => BindLiteralExpression((LiteralExpressionSyntax)syntax),
+                SyntaxKind.UnaryExpression => BindUnaryExpression((UnaryExpressionSyntax)syntax),
+                SyntaxKind.BinaryExpression => BindBinaryExpression((BinaryExpressionSyntax)syntax),
+                _ => throw new Exception($"Unexpected syntax {syntax.Kind}")
+            };
+        }
+        private BoundExpression BindLiteralExpression(LiteralExpressionSyntax syntax)
+        {
+            var value = syntax.LiteralToken.Value as int? ?? 0;
+
+            return new BoundLiteralExpression(value);
+        }
+        private BoundExpression BindUnaryExpression(UnaryExpressionSyntax syntax)
+        {
+            var boundOperatorKind = BindUnaryOperatorKind(syntax.OperatorToken.Kind);
+            var boundOperand = BindExpression(syntax.Operand);
+
+            return new BoundUnaryExpression(boundOperatorKind, boundOperand);
+        }
+
+        private BoundExpression BindBinaryExpression(BinaryExpressionSyntax syntax)
+        {
+            var boundLeft = BindExpression(syntax.Left);
+            var boundOperatorKind = BindBinaryOperatorKind(syntax.OperatorToken.Kind);
+            var boundRight = BindExpression(syntax.Right);
+
+            return new BoundBinaryExpression(boundLeft, boundOperatorKind, boundRight);
+        }
+
+        private BoundUnaryOperatorKind BindUnaryOperatorKind(SyntaxKind kind)
+        {
+            return kind switch
+            {
+                SyntaxKind.PlusToken => BoundUnaryOperatorKind.Identity,
+                SyntaxKind.MinusToken => BoundUnaryOperatorKind.Negation,
+                _ => throw new Exception($"Unexpected unary operator {kind}.")
+            };
+        }
+
+        private BoundBinaryOperatorKind BindBinaryOperatorKind(SyntaxKind kind)
+        {
+            return kind switch
+            {
+                SyntaxKind.PlusToken => BoundBinaryOperatorKind.Addition,
+                SyntaxKind.MinusToken => BoundBinaryOperatorKind.Subtraction,
+                SyntaxKind.StarToken => BoundBinaryOperatorKind.Multiplication,
+                SyntaxKind.SlashToken => BoundBinaryOperatorKind.Division,
+                _ => throw new Exception($"Unexpected binary operator {kind}."),
+            };
+        }
+    }
+}
